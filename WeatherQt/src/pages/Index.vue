ea7fbe8bb1263e9732b1677dcfc102bf4e484c22
Fabio Velasquez <fvela009@fiu.edu>
2021-12-25 13:55:22 -0500

    updating main page

---
+++ WeatherQt/src/pages/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="flex column">
+    <div class="col">
+     <q-input bottom-slots v-model="text" label="Label" counter maxlength="12" :dense="dense">
+        <template v-slot:before>
+          <q-icon name="event" />
+        </template>
+
+        <template v-slot:hint>
+          Field hint
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" />
+        </template>
+      </q-input>
+      </div>
+   
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
+
+<style lang="sass">
+ .q-page
+    background: linear-gradient(to bottom, #2980b9, #6dd5fa, #ffffff)
+
+</style>
