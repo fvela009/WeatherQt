3d12c626d77b259c240c76d33e9bc7561605e2ed
Fabio Velasquez <fvela009@fiu.edu>
2022-01-11 10:08:36 -0500

    updated weather api

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,9 @@
 <template>
-  <q-page class="flex column">
+  <q-page class="flex column" :class="bgClass">
     <div class="col q-pt-lg q-px-md">
      <q-input bottom-slots 
-        v-model="search" 
+        v-model="search"
+        @keyup.enter="getWeatherBySearch"
         placeholder="Search"
         dark
         borderless 
@@ -15,7 +16,12 @@
         </template>
 
         <template v-slot:append>
-          <q-btn round dense flat icon="search" />
+          <q-btn 
+          @click="getWeatherBySearch"
+          round 
+          dense 
+          flat 
+          icon="search" />
         </template>
       </q-input>
       </div>
@@ -24,14 +30,14 @@
 
       <div class="col text-white text-center">
         <div class="text-h4 text-weight-light">
-          Miami
+          {{ weatherData.name }}
         </div>
         <div class="text-h6 text-weight-light">
-          Rainy
+          {{ weatherData.weather[0].main }}
         </div>
         <div class="text-h1 text-weight-thin q-my-lg relative-position">
-          <span>8</span>
-          <span class="text-h4 relative-position degree">&deg;</span>
+          <span>{{ Math.round(weatherData.main.temp) }}</span>
+          <span class="text-h4 relative-position degree">&deg;C</span>
         </div>
       </div>
       
@@ -39,7 +45,7 @@
         </div>
 
         <div class="col text-center">
-          <img src= "https://www.fillmurray.com/100/100" alt="Bill">
+          <img :src="`http://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`">
         </div>
         </template>
 
@@ -72,18 +78,61 @@ export default defineComponent({
       search: '',
       weatherData: null,
       lat: null,
-      lon: null
+      lon: null,
+      apiURL: 'http://api.openweathermap.org/data/2.5/weather',
+      apiKey: '8d091c0d55b64e59189de819a1a4c90f'
+    }
+  },
+  computed: {
+    bgClass() {
+      if (this.weatherData) {
+        if(this.weatherData.weather[0].icon.endsWith('n')) {
+          return 'bg-night'
+        }
+        else {
+          return 'bg-day'
+        }
+      }
     }
   },
   methods: {
     getLocation() {
+      this.$q.loading.show()
+      
+      if (this.$q.platform.is.electron){
+        this.$axios.get('https://freegeoip.app/json/').then(response => {
+          this.lat = response.data.latitude
+          this.lon = response.data.longitude
+          this.getWeatherByCoords
+        })
+      }
+      else {
       navigator.geolocation.getCurrentPosition
       (position => {
         console.log('position: ', position)
         this.lat = position.coords.latitude
-        this.long = position.coords.longitude
+        this.lon = position.coords.longitude
+        this.getWeatherByCoords()
       })
-    }
+      }
+    },
+    getWeatherByCoords() {
+      this.$q.loading.show()
+      this.$axios(`${ this.apiURL }?lat=${ this.lat }&lon=${ this.lon }&appid=${ this.apiKey }&units=metric`).then(response => {
+        this.weatherData = response.data
+        this.$q.loading.hide()
+      })
+
+    },
+     getWeatherBySearch() {
+      this.$q.loading.show()
+      this.$axios(`${ this.apiURL }?q=${ this.search }&appid=${ this.apiKey }&units=metric`).then(response => {
+         this.weatherData = response.data
+        this.$q.loading.hide()
+
+       })
+
+    },
   }
 })
 </script>
@@ -91,6 +140,10 @@ export default defineComponent({
 <style lang="sass">
   .q-page
     background: linear-gradient(to bottom, #136a8a, #267871)
+    &.bg-night
+      background: linear-gradient(to bottom, #232526, #414345)
+    &.bg-day
+    background: linear-gradient(to bottom, #00b4db, #0083b0)
   .degree 
     top: -44px
   .skyline
